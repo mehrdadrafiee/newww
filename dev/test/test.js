89147b6f44933965e214e165fa956b23267d0b4e
faiq <faiqrazarizvi@gmail.com>
2014-06-24 15:25:06 -0700

    init commit

---
+++ dev/test/test.js
@@ -0,0 +1,13 @@
+var request = require('request')
+, github2es = require('github2es')
+, config = require('../../config');
+
+request('http://localhost:15984/registry/_all_docs', function(err, res, body){ 
+  if (err){
+    console.log('error getting all docs'); 
+    return
+  }
+  body = JSON.parse(body); 
+  var worker = new github2es(body.rows, config.search.url, '03259061f0f5d7190e00e4d45e5b997014c3be2c');
+  worker.groupPackages();
+});
