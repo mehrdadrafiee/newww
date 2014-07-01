274f4fc8cdf7fc19673cc53ff13b41422e90f1fb
faiq <faiqrazarizvi@gmail.com>
2014-07-01 11:44:48 -0700

    changes 2 test

--- dev/test/test.js
+++ dev/test/test.js
@@ -11,3 +11,14 @@ request('http://localhost:15984/registry/_all_docs', function(err, res, body){
   var worker = new github2es(body.rows, config.search.url, '03259061f0f5d7190e00e4d45e5b997014c3be2c');
   worker.groupPackages();
 });
+/*var opt =  {
+  method: 'GET',
+  url: 'https://api.github.com/repos/mikeal/request',
+  headers: {
+  'User-Agent': 'request',
+  "Authorization": "token " + '03259061f0f5d7190e00e4d45e5b997014c3be2c'   }
+}
+
+request(opt, function(err, body, res) { 
+  console.log(body)
+});*/
