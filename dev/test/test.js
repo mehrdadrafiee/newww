6cb227cfabe51d39f231b5295aa3c284b3d47b00
faiq <faiqrazarizvi@gmail.com>
2014-07-09 14:49:00 -0700

    small issues

--- dev/test/test.js
+++ dev/test/test.js
@@ -1,5 +1,6 @@
 var request = require('request')
 , github2es = require('github2es')
+, path = require('path') 
 , config = require('../../config');
 
 request('http://localhost:15984/registry/_all_docs', function(err, res, body){ 
@@ -8,17 +9,5 @@ request('http://localhost:15984/registry/_all_docs', function(err, res, body){
     return
   }
   body = JSON.parse(body); 
-  var worker = new github2es(body.rows, config.search.url, '03259061f0f5d7190e00e4d45e5b997014c3be2c');
-  worker.groupPackages();
+  var worker = new github2es(body.rows, config.search.url, '03713dc81ae441b677ebc8cee892e95586cd670', path.join(__dirname, 'sequence.seq'), function (){ console.log('done'); });
 });
-/*var opt =  {
-  method: 'GET',
-  url: 'https://api.github.com/repos/mikeal/request',
-  headers: {
-  'User-Agent': 'request',
-  "Authorization": "token " + '03259061f0f5d7190e00e4d45e5b997014c3be2c'   }
-}
-
-request(opt, function(err, body, res) { 
-  console.log(body)
-});*/
