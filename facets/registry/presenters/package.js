2b7eb15f9857fdfb75d8c84d47144042eef8c95f
Zeke Sikelianos <zeke@sikelianos.com>
2014-09-18 12:01:39 -0700

    remove nodei.co badges from readmes

--- facets/registry/presenters/package.js
+++ facets/registry/presenters/package.js
@@ -85,7 +85,7 @@ module.exports = function package (data, cb) {
     data.ghapi = gh(data.bugs.url).api_url
   }
 
-  removeRedundantHeadingFromReadme(data)
+  removeSuperfluousContentFromReadme(data)
 
   return cb(null, data);
 }
@@ -306,15 +306,23 @@ function processDependencies (dependencies, max) {
   return deps;
 }
 
-// If the README's first H1 element contains text that
-// matches the package name, then remove the element.
-function removeRedundantHeadingFromReadme (data) {
+function removeSuperfluousContentFromReadme (data) {
   if (typeof data.readme !== "string") return
   var $ = cheerio.load(data.readme)
+
+  // Remove first H1 if it matches package name
   var h1 = $('h1').first()
   var namePattern = new RegExp(data.name, "i")
   if (h1 && h1.text() && h1.text().match(namePattern)) {
     h1.remove()
     data.readme = $.html()
   }
+
+  // Remove nodei.co badges
+  var badges = $("a[href*='//nodei.co']")
+  if (badges) {
+    badges.remove()
+    data.readme = $.html()
+  }
+
 }
